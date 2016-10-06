3da1abcf3d65f3fb14d54bef160164a6c8008495
Paul Defilippi <BlindFaith@pauls-imac.home>
2016-10-06 00:21:11 -0400

    Setup FIrebase and pods related

---
+++ Pods/Target Support Files/Pods-lets-get-social/Pods-lets-get-social-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_lets_get_socialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_lets_get_socialVersionString[];
+
---
+++ lets-get-social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  lets-get-social
+//
+//  Created by Paul Defilippi on 10/5/16.
+//  Copyright © 2016 Paul Defilippi. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
