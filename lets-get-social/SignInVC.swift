662c36bbb762a6fd9a58b12c82eefdf1046c5e3f
Paul Defilippi <BlindFaith@pauls-imac.home>
2016-10-11 00:31:15 -0400

    finished Facebook authorization

--- lets-get-social/SignInVC.swift
+++ lets-get-social/SignInVC.swift
@@ -7,19 +7,43 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func facebookBtnPressed(_ sender: AnyObject) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("PAUL: Unable to authenticate with Facebook - \(error)")
+                
+            } else if result?.isCancelled == true {
+                print("PAUL: User cancelled Facebook authentication")
+            } else {
+                print("PAUL: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+            }
+            
+        }
     }
-
-
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("PAUL: Unable to authenticate with Firebase - \(error)")
+            } else {
+                print("PAUL: Successfully authenticated with Firebase")
+            }
+        })
+    }
 }
 
