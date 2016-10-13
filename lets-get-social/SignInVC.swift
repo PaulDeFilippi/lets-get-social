5cf47c909a62ca0c60ce76bc46b6326636464a1b
Paul Defilippi <BlindFaith@pauls-imac.home>
2016-10-13 16:29:51 -0400

    Completed creating FIrebase DB users

--- lets-get-social/SignInVC.swift
+++ lets-get-social/SignInVC.swift
@@ -53,7 +53,8 @@ class SignInVC: UIViewController {
             } else {
                 print("PAUL: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -66,7 +67,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("PAUL: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion:  { (user, error) in
@@ -75,7 +77,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("PAUL: Successfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
 
                             
@@ -87,7 +90,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("PAUL: Data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
