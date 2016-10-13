d4badb494bcd34ea23e6a2b374dc8bd41cea5149
Paul Defilippi <BlindFaith@pauls-imac.home>
2016-10-13 01:20:25 -0400

    completed feed UI version 1

---
+++ lets-get-social/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  lets-get-social
+//
+//  Created by Paul Defilippi on 10/13/16.
+//  Copyright © 2016 Paul Defilippi. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
